028d7da1588016a94a854d1157b3c6f48ad5b061
mayankanand7 <mayank@admavin.com>
2025-02-22 17:22:51 +0530

    [src]: added controller, security context xml, web.xml and log4j2 properties

---
+++ terror-movies-web/src/main/java/com/terrormovies/web/controller/AdminController.java
@@ -0,0 +1,24 @@
+package com.terrormovies.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdminController.class);
+
+
+    @RequestMapping(value = "/movies", method = RequestMethod.POST)
+    @ResponseBody
+    public String createMovie(@RequestBody String movie) {
+        LOGGER.info("Adding movie :: {}", movie);
+
+        return "created";
+    }
+
+}
