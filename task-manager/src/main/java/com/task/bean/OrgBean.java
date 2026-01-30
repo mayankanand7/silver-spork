1ce9ca809a63081599fbfc042517eaf61483c1f8
mayankanand7 <mayank@admavin.com>
2026-01-30 06:37:56 +0530

    [task-manager]: added bean classes for Org, User and Task

---
+++ task-manager/src/main/java/com/task/bean/OrgBean.java
@@ -0,0 +1,160 @@
+package com.task.bean;
+
+public class OrgBean {
+
+    // Fields
+    private Long id;
+    private Long version;
+
+    private String name;
+    private String description;
+
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+
+    private String phoneNumber;
+    private String email;
+
+    private Long createdAt;
+    private Long updatedAt;
+
+
+    // Constructor
+    // ------------------------------------------------------------------------
+
+    public OrgBean() {
+        super();
+    }
+
+
+    // Getters and Setters
+    // ------------------------------------------------------------------------
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Long updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+
+    // Object Methods
+    // ------------------------------------------------------------------------
+
+    @Override
+    public String toString() {
+        return "OrgBean [name=" + name + ", city=" + city + ", state=" + state + ", phoneNumber=" + phoneNumber
+                + ", email=" + email + "]";
+    }
+
+
+}
